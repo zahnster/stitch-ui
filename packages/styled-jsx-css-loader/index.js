9373d262ef5ceb43a566c39c8a4f3376b6ab8021
Jade Pennig <jpennig@twilio.com>
2021-07-25 14:13:16 -0500

    feat(wepack-styled-jsx-css-loader): new webpack loader that'll import and embed css into the styled jsx tag

---
+++ packages/styled-jsx-css-loader/index.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+module.exports = function (content) {
+  const basePath = this.resourcePath.match(/(.*)\/.*.[t|j]sx/)[1];
+  const lines = content.split(/\n/);
+
+  let cssVar = null;
+  let css = null;
+
+  const edited = lines.map((line) => {
+    const match = line.match(/import (.*) from "\.\/(.*).css";/);
+
+    if (match) {
+      cssVar = match[1];
+      css = fs
+        .readFileSync(`${basePath}/${match[2]}.css`, "utf-8")
+        .replace(/\n/g, "")
+        .replace(/\s/g, "")
+        .replace(/:root\s?{(.*?)}/, "$1");
+
+      return ""; // "erase" the line from the source code
+    }
+
+    if (cssVar) {
+      const varMatch = line.includes(`\$\{${cssVar}\}`);
+
+      if (varMatch) {
+        return css; // replace line w/ modified css
+      }
+    }
+
+    return line;
+  });
+
+  const editedString = edited.join("\n");
+
+  return editedString;
+};
