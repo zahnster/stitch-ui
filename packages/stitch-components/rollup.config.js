c84ea73c71237e35f35500a1514f0120c8e1e53f
Jade Pennig <jpennig@twilio.com>
2021-07-24 22:38:03 -0500

    feat(make-a-dist): add rollup and use to pack cjs dist for components

---
+++ packages/stitch-components/rollup.config.js
@@ -0,0 +1,29 @@
+import fastGlob from "fast-glob";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import multiInput from "rollup-plugin-multi-input";
+
+import wcToStyledJSX from "./lib/rollup-plugin-styled-jsx";
+
+const componentFiles = fastGlob.sync("./src/components/**/*.jsx", {
+  ignore: ["./src/components/**/*.stories.jsx"],
+});
+
+export default {
+  input: componentFiles,
+  output: {
+    dir: "dist",
+    format: "cjs",
+  },
+  plugins: [
+    multiInput(),
+    wcToStyledJSX(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/env", "@babel/react"],
+      plugins: ["styled-jsx/babel"],
+    }),
+    commonjs(),
+  ],
+  external: (id) => /^react|styled-jsx/.test(id),
+};
